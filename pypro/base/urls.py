d771333e465c44d640725ef416861f299ba8b0ae
Jairo97 <oliveirajairon10@gmail.com>
2022-07-26 21:17:33 -0300

    Resolução erro 400 heroku
    close #36

--- pypro/base/urls.py
+++ pypro/base/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 
-from pypro.base.views import home, testez
+from pypro.base.views import home
 
 
 app_name = 'base'
 urlpatterns = [
-    path('', home, name='home'),
-    path('testez',testez,name='testez')
+    path('', home, name="home"),
 ]
